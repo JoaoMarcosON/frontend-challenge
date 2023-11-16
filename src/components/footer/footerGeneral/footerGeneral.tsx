ff986a7a890220e65e20702dd9519a7a5b3c4307
JoaoMarcosON <joaomarcosnog@outlook.com>
2023-11-16 00:01:05 -0300

    feat: footer created and styled

---
+++ src/components/footer/footerGeneral/footerGeneral.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from './styled';
+
+export default function Footer() {
+    return (
+        <Container>
+            MKS sistemas © Todos os direitos reservados
+        </Container>
+    );
+};
---
+++ src/components/footer/footerGeneral/styled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.p`
+    background-color: #EEEEEE;
+    display: flex;
+    justify-content: center;
+    font-size: 1.2rem;
+    padding: 1rem;
+`;

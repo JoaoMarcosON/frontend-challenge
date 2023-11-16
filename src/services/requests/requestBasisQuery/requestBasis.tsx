06071f391c0c4edd56d1d81ef33c103ccd6a691e
JoaoMarcosON <joaomarcosnog@outlook.com>
2023-11-15 23:49:32 -0300

    fix: adding protection to url using .env

--- src/services/requests/requestBasisQuery/requestBasis.tsx
+++ src/services/requests/requestBasisQuery/requestBasis.tsx
@@ -1,12 +1,14 @@
-
 import { ProductResponse, ProductsParams } from '../types/types';
 import { useQuery } from 'react-query';
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL as string;
+
 export default function RequestProducts ({ page, rows, sortBy, orderBy }: ProductsParams) {
+  const apiUrl = `${baseUrl}/api/v1/products?page=${page}&rows=${rows}&sortBy=${sortBy}&orderBy=${orderBy}`;
+
     return useQuery<ProductResponse, Error>(
         ['products', { page, rows, sortBy, orderBy }],
         async () => {
-          const apiUrl = `https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=${page}&rows=${rows}&sortBy=${sortBy}&orderBy=${orderBy}`;
           const response = await fetch(apiUrl);
           if (!response.ok) {
             throw new Error('Erro ao obter os produtos');
@@ -14,6 +16,4 @@ export default function RequestProducts ({ page, rows, sortBy, orderBy }: Produc
           return response.json();
         }
       );
-}
-
-
+};

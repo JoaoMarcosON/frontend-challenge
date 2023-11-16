2c5fabebc02d8a314064dfbfc5c9db2ebf1ee4f2
JoaoMarcosON <joaomarcosnog@outlook.com>
2023-11-15 23:41:32 -0300

    styled: updated productList style

--- src/components/cards/productList/styled.tsx
+++ src/components/cards/productList/styled.tsx
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: auto;
+`;
+
+export const Content = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(23%, 1fr));
-  gap: 20px;
-  
-  @media (max-width: 280px) {
-  grid-template-columns: repeat(auto-fill, minmax(32%, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(20%, 1fr));
+  gap: 30px;
+  padding: 1%;
+  @media (max-width: 780px) {
+    grid-template-columns: 2fr;
+    gap: 10px;
   }
 `;
